9bb29975cf12ed4adc41728f13537dab8c32df2e
Hubert Lewandowski <lewandowski.hubert486@gmail.com>
2022-11-04 11:53:42 +0100

    Add Field class

---
+++ Game/Field.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Shapes;
+
+namespace Solitaire.Game
+{
+    public enum FieldShapes
+    {
+        Square = 0,
+        Circle
+    }
+
+    public class Field
+    {
+        private readonly Point _location;
+        private readonly Shape _shape;
+
+        Field(int x, int y, FieldShapes shape)
+        {
+            _location = new Point(x, y);
+            _shape = shape switch
+            {
+                FieldShapes.Square => new System.Windows.Shapes.Rectangle(),
+                FieldShapes.Circle => new Ellipse(),
+                _ => throw new ArgumentOutOfRangeException(nameof(shape), shape, "The given shape is undefined!")
+            };
+        }
+    }
+}
